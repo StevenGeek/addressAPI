fcb1671d2f3542b5b08581eb534895a43c952319
steven <stevengeek@163.com>
2017-03-16 11:11:28 +0800

    测试类

--- address-service/src/test/java/com/service/address/app/test/AddressServiceTest.java
+++ address-service/src/test/java/com/service/address/app/test/AddressServiceTest.java
@@ -1,5 +1,8 @@
 package com.service.address.app.test;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.service.address.business.impl.AddressBusiness;
 import com.service.address.orm.bean.CityBean;
+import com.service.web.exception.ParameterException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,4 +26,9 @@ public class AddressServiceTest {
         System.out.println(cityList);
     }
 
+    @Test
+    public void testBaiduAddressQuery() throws IOException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException, ParameterException {
+        c_addressService.fuzzyQueryCityInfo("龙耀路");
+    }
 }
