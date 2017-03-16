b85ba85a160ffe585401ddb6a4c1a99acd3c76b4
steven <stevengeek@163.com>
2017-03-16 11:11:14 +0800

    属性配置

--- address-service/src/main/java/com/service/address/AddressApplication.java
+++ address-service/src/main/java/com/service/address/AddressApplication.java
@@ -2,11 +2,18 @@ package com.service.address;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.service.address.properties.BaiDuProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(BaiDuProperties.class)
+@ComponentScan("com.service")
 public class AddressApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AddressApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AddressApplication.class, args);
+    }
+
 }
